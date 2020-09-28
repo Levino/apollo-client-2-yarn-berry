8cbfec88359085e980d48f6cba0815206e71e583
Levin Keller <post@levinkeller.de>
2020-09-28 11:04:37 +0200

    Not working with berry

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { useQuery } from 'react-apollo'
+import gql from 'graphql-tag'
 function App() {
+  const { loading } = useQuery(gql`
+  {
+    user(id: 5) {
+      firstName
+      lastName
+    }
+  }
+`)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {!loading &&
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+import { MockedProvider } from '@apollo/react-testing'
+
+test('renders learn react link', () => {
+  const { getByText } = render(<MockedProvider mock={{}}><App /></MockedProvider>);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
